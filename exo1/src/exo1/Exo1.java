ccf0bf68a2b29f897e60e53599b30da9f6605ba7
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-09-18 12:56:46 +0200

    réalisation tp0 début

---
+++ exo1/src/exo1/Exo1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * clemenceamirault tdB, TP0, 08/12/2023
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
