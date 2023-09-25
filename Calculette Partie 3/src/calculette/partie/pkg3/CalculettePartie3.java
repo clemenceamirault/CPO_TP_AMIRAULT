b539995f4cd62d27bbcb802a5cb1df21f7939ff2
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-09-25 15:27:30 +0200

    rendu TP0 Partie 2

---
+++ Calculette Partie 3/src/calculette/partie/pkg3/CalculettePartie3.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculette.partie.pkg3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Amirault Clémence TP0 TDB 25/09/23
+ */
+public class CalculettePartie3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator:"+"\n1) add"+"\n2) substract"+"\n3) multiply"+"\n4) divide"+"\n5) modulo");
+        int nb;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("\n Entrer le nombre :");
+      nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+      
+      int operande1;
+              Scanner m1 = new Scanner(System.in);
+              System.out.println("\n Entrer le nombre :");
+              nb=m1.nextInt();
+    
+      int operande2;
+              Scanner m2 = new Scanner(System.in);
+              System.out.println("\n Entrer le nombre :");
+              nb=m2.nextInt(); 
+              
+       
+    }   
+   
+    
+}
+    
+
