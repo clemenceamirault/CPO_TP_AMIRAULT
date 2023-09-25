633199083c7edc96a23500977e4aeed86d3c9644
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-09-25 22:29:25 +0200

    rendu TP0 calculette

--- Calculette Partie 3/src/calculette/partie/pkg3/CalculettePartie3.java
+++ Calculette Partie 3/src/calculette/partie/pkg3/CalculettePartie3.java
@@ -16,24 +16,48 @@ public class CalculettePartie3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int operateur;
         System.out.println("Please enter the operator:"+"\n1) add"+"\n2) substract"+"\n3) multiply"+"\n4) divide"+"\n5) modulo");
         int nb;
       Scanner sc = new Scanner(System.in);
       System.out.println("\n Entrer le nombre :");
       nb=sc.nextInt(); // On demande a sc de donner le prochain entier
       
-      int operande1;
+      int operande1 = 0;
               Scanner m1 = new Scanner(System.in);
               System.out.println("\n Entrer le nombre :");
               nb=m1.nextInt();
     
-      int operande2;
+      int operande2 = 0;
               Scanner m2 = new Scanner(System.in);
               System.out.println("\n Entrer le nombre :");
               nb=m2.nextInt(); 
               
+       if (operateur= 1){
+           System.out.println(operande1 + operande2);
+       } else {
+       }
        
-    }   
+       if (operateur=2) {
+      System.out.println( operande1 - operande2);
+       }
+       
+       if (operateur=3) {
+       System.out.println( operande1 * operande2 );
+       }
+       
+       if (operateur=4) {
+       System.out.println( operande1 / operande2);
+       }
+       
+       if ( operateur = 5 ) {
+       System.out.println( operande1 % operande2);
+       }
+       
+       if ( 1< operateur && operateur > 5) {
+           System.out.println(" il faut choisir un nombre entre 1 et 5 ");
+       }
+    }
    
     
 }
