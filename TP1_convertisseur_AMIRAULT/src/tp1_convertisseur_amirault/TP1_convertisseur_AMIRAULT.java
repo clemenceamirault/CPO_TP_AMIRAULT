7d7e00ace2a797b6336fbe6d99f940bf7d498076
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-01 19:09:50 +0200

    RENDU TP1 PARTIE1 partie2

--- TP1_convertisseur_AMIRAULT/src/tp1_convertisseur_amirault/TP1_convertisseur_AMIRAULT.java
+++ TP1_convertisseur_AMIRAULT/src/tp1_convertisseur_amirault/TP1_convertisseur_AMIRAULT.java
@@ -18,27 +18,56 @@ public class TP1_convertisseur_AMIRAULT {
     public static void main(String[] args) {
         double tc;
         Scanner sc = new Scanner(System.in);
-      System.out.println("\n Entrer le nombre :");
-      tc=sc.nextDouble(); // On demande a sc de donner le prochain entier
+      System.out.println("\n Bonjour, saisissez une valeur:");
+      tc=sc.nextDouble(); // On demande a sc une valeur pour les degrés 
       
-        double tk;
-            System.out.println(tk = tc + 273);
+      System.out.println("Saisissez la conversion que vous souhaiter effectuer :");
+      
+       
+       Scanner sd = new Scanner(System.in);
+      System.out.println("\n 1- De Celcius vers Kelvin"+ "\n 2- De Kelvin vers Celcius" + "\n 3- De Farenheit vers celcius"+ "\n 4- De Kelvin vers Farenheit"+"\n 5- De Farenheit vers Kelvin" );
+      tc=sd.nextDouble(); // On demande a sd d'afficher qu'elle formule choisir
+       
         
         double app;
         app = CelciusVersKelvin(tc);
-        System.out.println(app);
+        System.out.println(app); // cela permet d'afficher le résultat du calcul
         
+        double tk;
+            System.out.println(tk = tc + 273);
+            
         double tf;
-               
+           System.out.println(tf = tk*9/5-459.67);
+        
+        double app5;
+        app5 = KelvinVersCelcius(tk);
+        System.out.println(app5);
+        
         double app2;
         app2 = FarenheitVersCelcius(tf);
         System.out.println(app2);
+        
+        double app3;
+        app3 = KelvinVersFarenheit(tk);
+        System.out.println(app3);
+        
+        double app4;
+        app4 = FarenheitVersKelvin(tf);
+        System.out.println(app4);
+        
+        
     }
     
     public static double CelciusVersKelvin(double tc) {
      double tk;
         tk = tc + 273;
         return tk;
+    }
+    
+    public static double KelvinVersCelcius(double tk){
+        double tc;
+        tc = tk - 273;
+        return tc;
     }
     
     public static double FarenheitVersCelcius(double tf) {
@@ -48,6 +77,15 @@ public class TP1_convertisseur_AMIRAULT {
     }
     
     public static double KelvinVersFarenheit (double tk) {
-        
+        double tf;
+           tf = tk*9/5-459.67;
+           return tk;      
+    }
+    
+    public static double FarenheitVersKelvin(double tf) {
+        double tk;
+        tk = (tf-32)/1.8000+273.15;
+        return tk;
+                
     }
 }
