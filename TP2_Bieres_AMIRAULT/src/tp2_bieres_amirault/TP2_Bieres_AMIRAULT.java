2bbcd278af808a044470faa09c65202dec62e958
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-12 15:28:16 +0200

    TP2

---
+++ TP2_Bieres_AMIRAULT/src/tp2_bieres_amirault/TP2_Bieres_AMIRAULT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_amirault;
+
+/**
+ *
+ * @author Amirault TDB TP2 12/10/2023
+ */
+public class TP2_Bieres_AMIRAULT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+    }
+    
+}
