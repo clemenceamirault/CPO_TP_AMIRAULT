42e1cbfacc5120c7957976759503e72ccc764c7b
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-12 17:22:52 +0200

    TP2 convertisseur objet

---
+++ TP2_convertisseurObjet_AMIRAULT/src/tp2_convertisseurobjet_amirault/Convertisseur.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_amirault;
+
+/**
+ *
+ * @author user
+ */
+public class Convertisseur {
+    int nbConversions;
+}
