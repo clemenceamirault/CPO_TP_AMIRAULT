6c82ba27b58e700374ea477583d3ce765ecb69c1
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-12 22:34:20 +0200

    TP2 convertisseur objet

--- TP2_convertisseurObjet_AMIRAULT/src/tp2_convertisseurobjet_amirault/Convertisseur.java
+++ TP2_convertisseurObjet_AMIRAULT/src/tp2_convertisseurobjet_amirault/Convertisseur.java
@@ -10,4 +10,26 @@ package tp2_convertisseurobjet_amirault;
  */
 public class Convertisseur {
     int nbConversions;
+    
+    public Convertisseur (){
+        nbConversions = 0;
+    }
+    
+        public void convertirFahrenheitEnCelsius(double fahrenheit) {
+        double celsius = (fahrenheit - 32) * 5/9;
+        nbConversions++;
+        System.out.println(fahrenheit + " degrés Fahrenheit équivalent à " + celsius + " degrés Celsius.");
+    }
+
+    public void convertirCelsiusEnFahrenheit(double celsius) {
+        double fahrenheit = (celsius * 9/5) + 32;
+        nbConversions++;
+        System.out.println(celsius + " degrés Celsius équivalent à " + fahrenheit + " degrés Fahrenheit.");
+    }
+    
+    @Override
+    public String toString () {
+        return "nb de conversions"+ nbConversions;
+    }
 }
+
