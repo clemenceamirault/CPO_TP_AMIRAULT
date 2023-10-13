b7431ecc63b1aa72b60ca11a925e5f5d54919433
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-13 10:51:35 +0200

    TP2 MANIP

---
+++  TP2_manip_AMIRAULT/src/tp2_manip_amirault/Tartiflette.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_amirault;
+
+/**
+ *
+ * @author user
+ */
+public class Tartiflette {
+    int nbCalories ;
+    
+public Tartiflette (int Calories){
+   Calories = nbCalories ; 
+}    
+
+public int getCalories (){
+    return nbCalories;
+}
+
+public void incrementeCalories (int Calories){
+    nbCalories += Calories;
+}
+
+}
