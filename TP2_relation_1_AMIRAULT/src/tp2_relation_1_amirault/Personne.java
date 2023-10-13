f46fec32c0fbddd432ffb4049dcf45c29ef6a23e
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-13 16:26:32 +0200

    TP2 RELATION1

--- TP2_relation_1_AMIRAULT/src/tp2_relation_1_amirault/Personne.java
+++ TP2_relation_1_AMIRAULT/src/tp2_relation_1_amirault/Personne.java
@@ -9,5 +9,30 @@ package tp2_relation_1_amirault;
  * @author user
  */
 public class Personne {
+    String Nom;
+    String Prenom;
+    int nbVoiture = 0;
+    Voiture[] liste_Voitures;
     
+    public Personne(String unNom, String unPrenom){
+        Nom = unNom;
+        Prenom = unPrenom;
+        liste_Voitures = new Voiture[3];
+    }
+    
+    public boolean nouvelle_voiture (Voiture voiture_en_plus){
+        boolean x = true;
+        if (nbVoiture == 3){
+            x = false;
+        }
+        
+        if (voiture_en_plus.proprietaire != null){
+            x = false;
+        }
+        
+        liste_Voitures[nbVoiture] = voiture_en_plus;
+        nbVoiture++ ;
+        voiture_en_plus.proprietaire = this;
+        return x;
+    }
 }
