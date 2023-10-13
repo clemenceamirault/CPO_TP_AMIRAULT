39306704439c253d1d10c6544b27fa00e14f7cdd
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-13 16:38:37 +0200

    TP2 RELATION1

--- TP2_relation_1_AMIRAULT/src/tp2_relation_1_amirault/TP2_relation_1_AMIRAULT.java
+++ TP2_relation_1_AMIRAULT/src/tp2_relation_1_amirault/TP2_relation_1_AMIRAULT.java
@@ -14,7 +14,36 @@ public class TP2_relation_1_AMIRAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        
+        Personne bob = new Personne("Bobby", "Sixkiller");
+        Personne reno = new Personne("Reno", "Raines");
+        
+        System.out.println("liste des voitures disponibles "+ uneClio + 
+        "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+        bob.liste_Voitures[0] = uneClio ;
+        bob.liste_Voitures[1] = uneMicra;
+        bob.nbVoiture = 2 ;
+        uneClio.proprietaire = bob ;
+        uneMicra.proprietaire = bob; 
+        
+        System.out.println("la premiere voiture de Bob est " + 
+        bob.liste_Voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Bob est" + bob.liste_Voitures[1]);
+
+        reno.liste_Voitures[0] = une2008;
+        reno.liste_Voitures [1] = uneAutreClio;
+        reno.nbVoiture = 2;
+        une2008.proprietaire = reno;
+        uneAutreClio.proprietaire = reno;
+        
+        System.out.println("la premiere voiture de Reno est " + 
+        reno.liste_Voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Reno est" + reno.liste_Voitures[1]);
     }
     
 }
