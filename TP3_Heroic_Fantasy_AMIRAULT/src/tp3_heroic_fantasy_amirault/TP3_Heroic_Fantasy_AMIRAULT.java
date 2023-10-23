d5a5e04858dfa39eae3441cee88362a1d1f20f53
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-23 12:27:36 +0200

    TP3

--- TP3_Heroic_Fantasy_AMIRAULT/src/tp3_heroic_fantasy_amirault/TP3_Heroic_Fantasy_AMIRAULT.java
+++ TP3_Heroic_Fantasy_AMIRAULT/src/tp3_heroic_fantasy_amirault/TP3_Heroic_Fantasy_AMIRAULT.java
@@ -4,6 +4,8 @@
  */
 package tp3_heroic_fantasy_amirault;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author TDB TP3 AMIRAULT Clémence 23/10/2023
@@ -14,7 +16,25 @@ public class TP3_Heroic_Fantasy_AMIRAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Arme> armeList = new ArrayList();
+        
+        Epee Excalibur = new Epee("Excalibur", 7, 5);
+        Epee Durandal = new Epee("Durandal", 4,7);
+        
+        Baton Chene = new Baton("Chene",4,5);
+        Baton Charme = new Baton("Charme",5,6);
+        
+        Arme[] tabArme =new Arme [4];
+        
+        armeList.add(Excalibur);
+        armeList.add(Durandal);
+        armeList.add(Chene);
+        armeList.add(Charme);
+        
+        for (int i=0; i<armeList.size(); i++){
+        Arme arme =armeList.get(i);
+        System.out.println(arme);
+        }
     }
     
 }
