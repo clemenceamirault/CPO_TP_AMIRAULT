bea0abd5a437e8c575fbd8be6a784c7472c4c9e4
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-10-24 18:32:34 +0200

    TP3

---
+++ TP3_Heroic_Fantasy_AMIRAULT/src/etreVivant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public interface etreVivant {
+    void seFatiguer();
+    boolean estVivant();
+    void estAttaque();
+    
+}
