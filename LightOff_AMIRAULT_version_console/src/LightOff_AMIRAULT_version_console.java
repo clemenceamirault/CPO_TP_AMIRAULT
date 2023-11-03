2ecb982bcf9c15dafe711551d160caa7825f30b0
Clémence Amirault <clemence.amirault@epfedu.fr>
2023-11-03 23:02:41 +0100

    Mini projet avec début d'améliorations

--- LightOff_AMIRAULT_version_console/src/LightOff_AMIRAULT_version_console.java
+++ LightOff_AMIRAULT_version_console/src/LightOff_AMIRAULT_version_console.java
@@ -14,33 +14,15 @@ public class LightOff_AMIRAULT_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CelluleLumineuse Cellule1 = new CelluleLumineuse ();
-        System.out.println("Etat de la cellule1 : "+Cellule1);
+     
         
-        CelluleLumineuse Cellule2 = new CelluleLumineuse ();
-        Cellule2.activerCellule();
-        System.out.println("Etat de la cellule2 : "+ Cellule2);
-        
-        Cellule1.activerCellule();
-        System.out.println("Etat de la cellule après activation : "+ Cellule1);
-        
-        Cellule2.eteindreCellule();
-        System.out.println("Etat de la cellule après extinction : "+ Cellule2);
-        
-        boolean EstEteint1 = Cellule1.estEteint();
-        System.out.println("La cellule 1 est éteinte : "+ EstEteint1);
-        
-        boolean EstEteint2 = Cellule2.estEteint();
-        System.out.println("La cellule 2 est éteinte : "+ EstEteint2);
-        
-        // Crée une nouvelle grille de cellules lumineuses de 5 lignes et 5 colonnes
-        GrilleDejeu grille = new GrilleDejeu(5, 5);
+           Partie partie = new Partie(); // Crée un objet Partie
 
-        // Affiche la grille
-        System.out.println("Grille de cellules lumineuses :");
-        System.out.println(grille);
-        
-         
+        partie.initialiserPartie(); // Initialise la partie en mélangeant la grille
+        partie.lancerPartie(); // Lance le jeu LightOff
     }
-    
 }
+         
+    
+    
+
